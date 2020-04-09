7d2017a4c16207989d34bcc235d0a3190e7a7926
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-09 20:09:06 +0530

    Output

--- variables_special.sh
+++ variables_special.sh
@@ -6,3 +6,12 @@ echo "Quoted Values: $*"
 echo "Total Number of Parameters : $#"
 
 #The command-line arguments $1, $2, $3, ...$9 are positional parameters, with $0 pointing to the actual command, program, shell script, or function and $1, $2, $3, ...$9 as the arguments to the command.
+
+#$ bash variables_special.sh Soumyadip Chowdhury
+#File Name: variables_special.sh
+#First Parameter : Soumyadip
+#Second Parameter : Chowdhury
+#Quoted Values: Soumyadip Chowdhury
+#Quoted Values: Soumyadip Chowdhury
+#Total Number of Parameters : 2
+
