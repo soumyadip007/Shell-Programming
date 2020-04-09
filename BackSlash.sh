ecff93b1213c420575501da2564c0d86bba4255a
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-09 21:07:20 +0530

    Backsalsh

---
+++ BackSlash.sh
@@ -0,0 +1,48 @@
+a=10
+echo "Value of a is $a \n"
+echo -e "Value of a is $a \n"
+
+# 1	
+# \\
+
+# backslash
+
+# 2	
+# \a
+
+# alert (BEL)
+
+# 3	
+# \b
+
+# backspace
+
+# 4	
+# \c
+
+# suppress trailing newline
+
+# 5	
+# \f
+
+# form feed
+
+# 6	
+# \n
+
+# new line
+
+# 7	
+# \r
+
+# carriage return
+
+# 8	
+# \t
+
+# horizontal tab
+
+# 9	
+# \v
+
+# vertical tab
