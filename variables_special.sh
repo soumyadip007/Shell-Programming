f266471a80953074125174a49ab0a4c02c209100
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-09 20:07:53 +0530

    cla

---
+++ variables_special.sh
@@ -0,0 +1,8 @@
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+#The command-line arguments $1, $2, $3, ...$9 are positional parameters, with $0 pointing to the actual command, program, shell script, or function and $1, $2, $3, ...$9 as the arguments to the command.
