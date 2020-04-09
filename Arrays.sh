7fd9df209b5a9ac9e965f5c9024577b08b1a5770
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-09 20:20:02 +0530

    Arrays

---
+++ Arrays.sh
@@ -0,0 +1,11 @@
+NAME[0]="Soumyadip"
+NAME[1]="Chowdhury"
+NAME[2]="JAVA"
+NAME[3]="Spring"
+NAME[4]="AWS"
+echo "First Index: ${NAME[0]}"
+echo "Second Index: ${NAME[1]}"
+echo "First Index: ${NAME[*]}"
+echo "Second Index: ${NAME[@]}"
+
+#The difference between an array variable and a scalar variable can be explained as follows.
