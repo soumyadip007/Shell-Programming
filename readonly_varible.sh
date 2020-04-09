a6a3455096f4f40d6dc914d66a5ee955064e1484
soumyadip007 <soumyadip.note@GMAIL.COM>
2020-04-09 20:04:09 +0530

    readonly line

---
+++ readonly_varible.sh
@@ -0,0 +1,9 @@
+var1="Soumyadip"
+readonly var1
+var1=19
+
+echo $var1
+
+
+#Read-only Variables
+#Shell provides a way to mark variables as read-only by using the read-only command. After a variable is marked read-only, its value cannot be changed.
