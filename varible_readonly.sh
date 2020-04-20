8e1c2756299d34241e5ebe3d6b91334c8c53c441
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-04-20 13:20:47 +0530

    Variable rollback

--- varible_readonly.sh
+++ varible_readonly.sh
@@ -1,17 +1,5 @@
-read n
+var1="Soumyadip"
+readonly var1
+var1=19
 
-for ((i=1;i&lt;=n;++))
-do
-flag=0
-for ((j=2;j&lt;i;j++))
-do
- if [ 'expr $i % $j' -eq 0]
- then
- fi
- done
- if [$flag-eq 0]
- then
-== echo $i
- fi
-<br />
- done</span><br /
+echo $var1
