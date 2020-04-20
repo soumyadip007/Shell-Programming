376cc02c457e36e927d681b1ae0dc0ebf521d496
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-04-20 13:20:33 +0530

    Variable Readonly

--- varible_readonly.sh
+++ varible_readonly.sh
@@ -1,5 +1,17 @@
-var1="Soumyadip"
-readonly var1
-var1=19
+read n
 
-echo $var1
+for ((i=1;i&lt;=n;++))
+do
+flag=0
+for ((j=2;j&lt;i;j++))
+do
+ if [ 'expr $i % $j' -eq 0]
+ then
+ fi
+ done
+ if [$flag-eq 0]
+ then
+== echo $i
+ fi
+<br />
+ done</span><br /
